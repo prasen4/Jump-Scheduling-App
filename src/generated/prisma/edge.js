adeff72c35a93a9ce4b4263ba803a6549074a4d8
Prathik Senthil <prathiksenthil@gmail.com>
2025-05-13 19:44:10 -0400

    Add database migration and update render.yaml

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -225,7 +225,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -226,7 +226,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
